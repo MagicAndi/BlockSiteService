3e2032ff07d72a4ce2a4ac9b5ecd52a106c25524
Andy Parkhill <a.parkhill@qub.ac.uk>
2017-01-13 15:31:35 +0000

    Initial Visual Studio solution commit.

---
+++ ServiceConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Topshelf;
+
+namespace BlockSiteService
+{
+    internal static class ServiceConfiguration
+    {
+        internal static void Configure()
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<MyService>(service =>
+                {
+                    service.ConstructUsing(s => new MyService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                //Setup Account that window service use to run.  
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("BlockSiteService");
+                configure.SetDisplayName("BlockSite Service");
+                configure.SetDescription("A .NET Windows Service created to block unproductive websites. Built with Topshelf.");
+            });
+        }
+    }
+}
